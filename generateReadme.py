107f07411d726f4c22f161c699583bc700191c80
kuoted <cvsuser@163.com>
2021-03-13 23:20:25 +0800

    add "Building Machines That Learn and Think Like People.pdf" a stanford famous paper.

--- generateReadme.py
+++ generateReadme.py
@@ -4,7 +4,7 @@ import os
 import urllib
 
 """ generate readme.md """
-__author__ = 'Wang Zhe'
+__author__ = 'kuoTed'
 
 paper_class_map = {}
 paper_map = {}
@@ -47,7 +47,8 @@ for line in all_lines:
         paper_class_flag = 1
         paper_class_name = line[3:].strip()
 
-github_root = "https://github.com/wzhe06/Reco-papers/blob/master/"
+# github_root = "https://github.com/wzhe06/Reco-papers/blob/master/"
+gitee_root = "https://gitee.com/cvsuser/DeepLearningPapers/blob/master/"
 all_dir = os.listdir("./")
 for one_dir in all_dir:
     if os.path.isdir(one_dir) and not one_dir.startswith('.'):
@@ -57,7 +58,7 @@ for one_dir in all_dir:
         all_sub_files = os.listdir(one_dir)
         for one_file in all_sub_files:
             if not os.path.isdir(one_file) and not one_file.startswith('.'):
-                out_file.write("* [" + ('.').join(one_file.split('.')[:-1]) + "]("+github_root + urllib.quote(one_dir.strip())+"/"
+                out_file.write("* [" + ('.').join(one_file.split('.')[:-1]) + "]("+gitee_root + urllib.quote(one_dir.strip())+"/"
                                + urllib.quote(one_file.strip())+") <br />\n")
                 if one_file.strip() in paper_map:
                     out_file.write(paper_map[one_file.strip()] + "\n")
